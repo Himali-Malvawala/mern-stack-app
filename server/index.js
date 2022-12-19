bb91e5432d276cbff7181a78b39f238af05cff85
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-19 19:01:28 +0530

    require books routes in index

--- server/index.js
+++ server/index.js
@@ -2,12 +2,16 @@ const express = require("express");
 require("dotenv").config({ path: "./config.env" });
 const connectDB = require("./database/db");
 
+const books = require("./routes/api/books");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 app.get("/", (req, res) => res.send("Hello Newyorkers!!"));
+
+app.use("/api/books", books);
 
 app.listen(port, () => {
   console.log(`server is running on port: ${port}`);
