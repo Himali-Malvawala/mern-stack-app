bdff8f0757148976e5c82cf552cb8763a3427789
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-19 14:52:03 +0530

    connect db in index

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 require("dotenv").config({ path: "./config.env" });
+const connectDB = require("./database/db");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 app.get("/", (req, res) => res.send("Hello Newyorkers!!"));
 
