2cecb0a96acead64fa65f0cc01fac3cf9ef13394
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-19 21:45:27 +0530

    create different routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import AllBooks from "./components/AllBooks";
+import BookDetails from "./components/BookDetails";
+import CreateBook from "./components/CreateBook";
+import UpdateBookInfo from "./components/UpdateBookInfo";
+
 const App = () => {
   return (
     <div>
       <header>
         <div>
           <p>okay here we go</p>
+          <Routes>
+            <Route exact path="/" element={<AllBooks />} />
+            <Route path="/create-book" element={<CreateBook />} />
+            <Route path="edit-book/:id" element={<UpdateBookInfo />} />
+            <Route path="/show-book/:id" element={<BookDetails />} />
+          </Routes>
         </div>
       </header>
     </div>
