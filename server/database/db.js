dd41c452e7cd3b9c9a240146e22610c060e78685
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-19 15:11:15 +0530

    solve strictQuery and await issue

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,8 @@ require("dotenv").config({ path: "../config.env" });
 
 const connectDB = async () => {
   try {
-    mongoose.connect(process.env.DB_URL);
+    mongoose.set("strictQuery", true);
+    await mongoose.connect(process.env.DB_URL);
     console.log("MongoDB is connected...");
   } catch (err) {
     console.log(err.message);
