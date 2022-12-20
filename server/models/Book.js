4d59805f2edd5fe848970125cda9330576170f75
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-20 14:38:46 +0530

    add image in schema

--- server/models/Book.js
+++ server/models/Book.js
@@ -13,6 +13,10 @@ const BookSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  image: {
+    type: String,
+    required: true,
+  },
   description: {
     type: String,
   },
