83beab8c155559718ba88b9a15628191d29564ee
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-21 12:44:31 +0530

    style UpdateBookInfo.js

--- client/src/components/UpdateBookInfo.js
+++ client/src/components/UpdateBookInfo.js
@@ -63,7 +63,7 @@ const UpdateBookInfo = () => {
   };
 
   return (
-    <div>
+    <div className="sm:flex sm:flex-col sm:justify-center sm:items-center mx-5">
       <Link
         to="/"
         className="bg-[#FFFFD0] hover:bg-transparent border-2 border-[#FFFFD0] px-3 py-2 text-lg font-semibold rounded-md hover:text-white text-left mr-auto text-[#A555EC]"
@@ -71,81 +71,109 @@ const UpdateBookInfo = () => {
         Show All Books
       </Link>
       <div>
-        <h1>Edit Book</h1>
-        <h5>Update Info</h5>
+        <h1 className="font-semibold text-5xl text-white mt-5">Edit Book</h1>
+        <h5 className="text-lg text-white mt-3 sm:text-center">Update Info</h5>
       </div>
       <div>
         <form className="mt-5" onSubmit={onSubmitHandler} noValidate>
           <div className="flex flex-col gap-5">
-            <label htmlFor="title">Title</label>
-            <input
-              type="text"
-              placeholder="Title"
-              name="title"
-              className="py-2 px-3 rounded-md shadow-2xl mb-5 sm:w-[30rem]"
-              value={book.title}
-              onChange={onChangeHandler}
-            />
-            <label htmlFor="isbn">ISBN</label>
+            <div className="flex flex-col">
+              <label htmlFor="title" className="mb-1 text-white">
+                Title
+              </label>
+              <input
+                type="text"
+                placeholder="Title"
+                name="title"
+                className="py-2 px-3 rounded-md shadow-2xl mb-5 sm:w-[30rem]"
+                value={book.title}
+                onChange={onChangeHandler}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="isbn" className="mb-1 text-white">
+                ISBN
+              </label>
 
-            <input
-              type="text"
-              placeholder="ISBN"
-              name="isbn"
-              className="py-2 px-3 rounded-md shadow-2xl sm:w-[30rem]"
-              value={book.isbn}
-              onChange={onChangeHandler}
-            />
-            <label htmlFor="author">Author</label>
+              <input
+                type="text"
+                placeholder="ISBN"
+                name="isbn"
+                className="py-2 px-3 rounded-md shadow-2xl sm:w-[30rem]"
+                value={book.isbn}
+                onChange={onChangeHandler}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="author" className="mb-1 text-white">
+                Author
+              </label>
 
-            <input
-              type="text"
-              placeholder="Author"
-              name="author"
-              className="py-2 px-3 rounded-md shadow-2xl sm:w-[30rem]"
-              value={book.author}
-              onChange={onChangeHandler}
-            />
-            <label htmlFor="image">Image Link</label>
+              <input
+                type="text"
+                placeholder="Author"
+                name="author"
+                className="py-2 px-3 rounded-md shadow-2xl sm:w-[30rem]"
+                value={book.author}
+                onChange={onChangeHandler}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="image" className="mb-1 text-white">
+                Image Link
+              </label>
 
-            <input
-              type="text"
-              placeholder="Image Link"
-              name="image"
-              className="py-2 px-3 rounded-md shadow-2xl sm:w-[30rem]"
-              value={book.image}
-              onChange={onChangeHandler}
-            />
-            <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                placeholder="Image Link"
+                name="image"
+                className="py-2 px-3 rounded-md shadow-2xl sm:w-[30rem]"
+                value={book.image}
+                onChange={onChangeHandler}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="description" className="mb-1 text-white">
+                Description
+              </label>
 
-            <input
-              type="text"
-              placeholder="Description"
-              name="description"
-              className="py-2 px-3 rounded-md shadow-2xl sm:w-[30rem]"
-              value={book.description}
-              onChange={onChangeHandler}
-            />
-            <label htmlFor="published_date">Published Date</label>
+              <input
+                type="text"
+                placeholder="Description"
+                name="description"
+                className="py-2 px-3 rounded-md shadow-2xl sm:w-[30rem]"
+                value={book.description}
+                onChange={onChangeHandler}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="published_date" className="mb-1 text-white">
+                Published Date
+              </label>
 
-            <input
-              type="date"
-              placeholder="Published Date"
-              name="published_date"
-              className="py-2 px-3 rounded-md shadow-2xl sm:w-[30rem]"
-              value={book.published_date}
-              onChange={onChangeHandler}
-            />
-            <label htmlFor="publisher">Publisher</label>
+              <input
+                type="date"
+                placeholder="Published Date"
+                name="published_date"
+                className="py-2 px-3 rounded-md shadow-2xl sm:w-[30rem]"
+                value={book.published_date}
+                onChange={onChangeHandler}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="publisher" className="mb-1 text-white">
+                Publisher
+              </label>
 
-            <input
-              type="text"
-              placeholder="Publisher"
-              name="publisher"
-              className="py-2 px-3 rounded-md shadow-2xl sm:w-[30rem]"
-              value={book.publisher}
-              onChange={onChangeHandler}
-            />
+              <input
+                type="text"
+                placeholder="Publisher"
+                name="publisher"
+                className="py-2 px-3 rounded-md shadow-2xl sm:w-[30rem]"
+                value={book.publisher}
+                onChange={onChangeHandler}
+              />
+            </div>
           </div>
           <button
             type="submit"
