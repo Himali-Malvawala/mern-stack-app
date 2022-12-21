1465fc0ed33493576f87e3b46b7fb54dd2a19a48
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-21 12:29:05 +0530

    style BookDetails.js

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -30,31 +30,39 @@ const BookDetails = () => {
   };
 
   return (
-    <div>
+    <div className="mx-5">
       <Link
-        to="/create-book"
+        to="/"
         className="bg-[#FFFFD0] hover:bg-transparent border-2 border-[#FFFFD0] px-3 py-2 text-lg font-semibold rounded-md hover:text-white text-left mr-auto text-[#A555EC]"
       >
-        + Create Book
+        Show All Books
       </Link>
-      <div>
-        <h1>{book.title}</h1>
-        <h6>{book.author}</h6>
-        <p>{book.isbn}</p>
-        <p>{book.publisher}</p>
-        <p>{book.published_date}</p>
-        <p>{book.description}</p>
+      <div className="mt-8 text-white">
+        <img
+          src={book.image}
+          alt={book.title}
+          className="sm:w-[450px] sm:h-auto rounded-md shadow-xl"
+        />
+        <h1 className="text-3xl font-semibold mt-5">{book.title}</h1>
+        <h6 className="text-lg">{book.author}</h6>
+        <p className="font-light mt-3">{book.publisher}</p>
+        <p className="font-light mt-1">ISBN: {book.isbn}</p>
+        <p className="font-light mt-1">{book.published_date}</p>
+        <p className="font-light mt-1">{book.description}</p>
       </div>
-      <div>
+      <div className="flex justify-between items-center sm:w-[450px] gap-6 mt-8">
         <button
           type="button"
           onClick={() => {
             onDelete(book._id);
           }}
+          className="border-2 border-[#FFFFD0] text-white py-1 grow rounded-md hover:bg-[#FFFFD0] hover:text-[#A555EC]"
         >
           Delete
         </button>
-        <Link to={`/edit-book/${book._id}`}>Edit</Link>
+        <div className="border-2 border-[#FFFFD0] text-white py-1 grow text-center rounded-md hover:bg-[#FFFFD0] hover:text-[#A555EC] cursor-pointer">
+          <Link to={`/edit-book/${book._id}`}>Edit</Link>
+        </div>
       </div>
     </div>
   );
