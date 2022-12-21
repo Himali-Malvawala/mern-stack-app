7514847179e2eac8bc683ea97c1bc4addd113bcd
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-21 14:32:36 +0530

    give group hover to cards

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -4,11 +4,11 @@ const Card = (props) => {
   const book = props.data;
   return (
     <div>
-      <div className="max-w-xs bg-[#FFFFD0] rounded-md">
+      <div className="max-w-xs bg-[#FFFFD0] rounded-md shadow-2xl group">
         <img src={book.image} alt={book.title} className="rounded-t-md" />
         <div className="px-2 py-2">
           <div className="sm:flex sm:justify-between sm:items-center">
-            <h4 className="text-xl font-medium text-slate-800 hover:text-[#A555EC] hover:underline underline-offset-2">
+            <h4 className="text-xl font-medium text-slate-800 group-hover:text-[#A555EC] group-hover:underline underline-offset-2">
               <Link to={`/show-book/${book._id}`}>{book.title}</Link>
             </h4>
             <h4 className="text-base text-slate-800 mt-1 sm:mt-0">
