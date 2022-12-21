6dd32211ca339683e4606e1aea2aa607dbeef27c
Himali Malvawala <himalimalvawala@gmail.com>
2022-12-21 12:00:40 +0530

    style card.js

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
 
 const Card = (props) => {
-  const book = props.book;
-
+  const book = props.data;
   return (
     <div>
-      <img src={book.image} alt={book.title} height={100} />
-      <div>
-        <h4>
-          <Link to={`/show-book/${book._id}`}>{book.title}</Link>
-        </h4>
-        <h4>{book.author}</h4>
-        <p>{book.description}</p>
+      <div className="max-w-xs bg-[#FFFFD0] rounded-md">
+        <img src={book.image} alt={book.title} className="rounded-t-md" />
+        <div className="px-2 py-2">
+          <div className="sm:flex sm:justify-between sm:items-center">
+            <h4 className="text-xl font-medium text-slate-800 hover:text-[#A555EC] hover:underline underline-offset-2">
+              <Link to={`/show-book/${book._id}`}>{book.title}</Link>
+            </h4>
+            <h4 className="text-base text-slate-800 mt-1 sm:mt-0">
+              {book.author}
+            </h4>
+          </div>
+          <p className="font-light mt-2">{book.description}</p>
+        </div>
       </div>
     </div>
   );
